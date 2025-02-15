8542d13f3fdb7c9a091331b991645dfdf874df5c
Akshat-paul <akshatpaul2006@gmail.com>
2025-02-15 21:54:08 +0530

    Added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+
+// src/App.jsx
+import React from 'react';
+import HomePage from './components/HomePage';
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
